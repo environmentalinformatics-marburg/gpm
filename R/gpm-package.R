fd69ac617438292f37cca716636f170f39b262a5
tim-salabim <tim.appelhans@gmail.com>
2015-06-08 16:36:06 +0200

    UPDATE: included function to sample a minimum training set according to a factor column of a spatial df object

---
+++ R/gpm-package.R
@@ -0,0 +1,19 @@
+#' Geospatial predictive modeling using parameterized and unparameterized models
+#'
+#' This package provides convenience functions for geospatial 
+#' predictive modeling of raster and saetllite data.
+#'
+#' @name gpm-package
+#' @aliases gpm
+#' @docType package
+#' @title Geospatial predictive modeling using parameterized and unparameterized models
+#' @author Tim Appelhans, Thomas Nauss, Hanna Meyer\cr
+#' \cr
+#' \emph{Maintainer:} Environmental Informatics \email{admin@@environmentalinformatics-marburg.de}
+#'
+#' @import raster
+#' @import satellite 
+#' 
+#' @keywords package
+#'
+NULL
---
+++ R/sampleBy.R
@@ -0,0 +1,56 @@
+#' sample a Spatial*DataFrame by according to a factorial column 
+#'
+#' @description
+#' This function samples a Spatial*DataFrame according to a specified
+#' column so that all factor levels are sampled \code{n} times. If there
+#' are less or equal entries in any levels these are sampled completely.
+#' 
+#' @param x a Spatial*DataFrame
+#' @param col the column used for the sampling
+#' @param n the amount of samples to be taken from each level of col
+#' @param seed an optional seed to be used for the 
+#' random \code{\link{sample}}-ing.
+#' @param ... additional arguments passed to \code{\link{sample}}, 
+#' e.g. \code{drop = ...}
+#'
+#' @return a list with entries \emph{sample_set} (the samples) 
+#' and \emph{remainder} (the remainder).
+#'
+#' @export sampleBy
+#' 
+#' @seealso \code{\link{sample}} for details on the random sampling
+#' 
+#' @examples
+#' # Not run
+#' library(sp)
+#' 
+#' data(meuse)
+#' coordinates(meuse) <- ~ x + y
+#' 
+#' sampleBy(meuse, col = "landuse", n = 1)
+#' 
+sampleBy <- function(x, col, n = 1, seed = NULL, ...) {
+  
+  s <- sp::split(x, x@data[, col], ...)
+  
+  indx <- unlist(lapply(seq(s), function(l) {
+    if (nrow(s[[l]]) > n) {
+      v <- rep(FALSE, nrow(s[[l]]))
+      if (is.null(seed)) set.seed(l) else set.seed(seed)
+      v[sample(seq(v), n)] <- TRUE
+      return(v)
+    } else {
+      if (nrow(s[[l]]) == n) return(TRUE)
+    }
+  }))
+  
+  ind <- order(x@data[, col])
+  x <- x[ind, ]
+  
+  set <- x[indx, ]
+  rest <- x[!indx, ]
+  
+  return(list(sample_set = set,
+              remainder = rest))
+  
+}
