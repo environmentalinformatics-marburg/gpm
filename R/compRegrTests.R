3224d80d37baa5e7097b7aa981e60d98c4620b05
tnauss <nauss@staff.uni-marburg.de>
2018-06-22 16:45:24 +0200

    Adjust compRegrTests for new ffs etc.

--- R/compRegrTests.R
+++ R/compRegrTests.R
@@ -30,123 +30,44 @@
 #' #Not run
 #' }
 #' 
-compRegrTests <- function(models, per_model = TRUE, per_selector = FALSE,
-                          sub_selectors = NULL, details = TRUE){
+compRegrTests = function(models, per_model = TRUE, mids = 1){
   #lapply(models, function(x){
   if(per_model == TRUE){
-    lst_models <- lapply(models, function(m){
-      lst_per_predictor <- lapply(m, function(r){
-        if(all(is.na(r$testing[[1]]))){
-          lpp <- NULL
+    mids = seq(length(models))
+  }
+  lst_models = lapply(mids, function(mi){
+    m = models[[mi]]
+    lst_per_response = lapply(m, function(r){
+      lst_per_sample = lapply(r, function(s){
+        if(inherits(s$model, "try-error")){
+          lpp = NULL
+        } else if(!is.null(s$testing[[1]])){
+          lpp = "Implement case in compRegTests."
         } else {
-          if(class(r$testing$PREDICTED) == "data.frame"){
-            r$testing$PREDICTED <- r$testing$PREDICTED[,1]
+          model_response = s$response
+          
+          #rfe
+          if(!is.null(s$model$fit)){
+            lpp = list(data.frame(model_response = s$response,
+                                  s$model$fit$results[s$model$fit$bestTune$ncomp, ]))
+            lpp[[2]] = data.frame(model_response = s$response,
+                                  s$model$fit$resample) 
+          } else {
+            lpp = list(data.frame(model_response = s$response,
+                                  s$model$results[s$model$bestTune$ncomp, ]))
+            lpp[[2]] = data.frame(model_response = s$response,
+                                  s$model$resample[s$model$resample$ncomp==s$model$bestTune$ncomp,])    
           }
-          lpp <- data.frame(model_response = r$response,
-                            model_selector = r$testing$SELECTOR,
-                            testing_response = r$testing$RESPONSE,
-                            testing_predicted = as.numeric(r$testing$PREDICTED))
+          rownames(lpp[[1]]) = NULL
+          rownames(lpp[[2]]) = NULL
         }
         return(lpp)
       })
-      
-      if(is.null(unlist(lst_per_predictor))){
-        lst <- NULL
-      } else {
-        if(per_selector == TRUE){
-          if(is.null(sub_selectors)){
-            sub_selectors <- c(1, 1000000L)
-          }
-          
-          lst_per_predictor <- do.call("rbind", lst_per_predictor)
-          selectors <- unique(substr(lst_per_predictor$model_selector, 
-                                     sub_selectors[1], sub_selectors[2]))
-          
-          lst_per_selector <- lapply(selectors, function(s){
-            pred <- lst_per_predictor$testing_predicted[
-              substr(lst_per_predictor$model_selector, 
-                     sub_selectors[1], sub_selectors[2]) == 
-                substr(s, sub_selectors[1], sub_selectors[2])]
-            resp <- lst_per_predictor$testing_response[
-              substr(lst_per_predictor$model_selector, 
-                     sub_selectors[1], sub_selectors[2]) == 
-                substr(s, sub_selectors[1], sub_selectors[2])]
-            
-            smmry <- summary(lm(pred~resp))
-            rmse = sqrt(mean((pred - resp)**2))
-            rmse_norm = rmse / mean(resp)
-            data.frame(model_response = 
-                         lst_per_predictor$model_response[
-                           substr(lst_per_predictor$model_selector,
-                                  sub_selectors[1], sub_selectors[2]) == 
-                             substr(s, sub_selectors[1], sub_selectors[2])],
-                       model_selector = s,
-                       testing_response = resp,
-                       testing_predicted = pred,
-                       pairs = length(pred),
-                       r_squared = smmry$r.squared,
-                       adj_r_squared = smmry$adj.r.squared,
-                       residuals = smmry$residuals,
-                       rmse = rmse,
-                       rmse_norm = rmse_norm)
-          })
-          lst <- do.call("rbind", lst_per_selector)
-        } else {
-          lst <- lapply(seq(length(lst_per_predictor)), function(s){
-            if(!is.null(lst_per_predictor[[s]])){
-              smmry <- summary(lm(lst_per_predictor[[s]]$testing_predicted ~ 
-                                    lst_per_predictor[[s]]$testing_response))
-              rmse = sqrt(mean((lst_per_predictor[[s]]$testing_predicted - lst_per_predictor[[s]]$testing_response)**2))
-              rmse_norm = rmse / mean(lst_per_predictor[[s]]$testing_response)
-              lst_per_predictor[[s]]$r_squared <- smmry$r.squared
-              lst_per_predictor[[s]]$adj_r_squared <- smmry$adj.r.squared
-              lst_per_predictor[[s]]$residuals <- smmry$residuals
-              lst_per_predictor[[s]]$rmse <- rmse
-              lst_per_predictor[[s]]$rmse_norm <- rmse_norm
-              lst_per_predictor[[s]]$sample <- s
-            }
-            return(lst_per_predictor[[s]])
-          })
-          lst <- (do.call("rbind", lst))
-        }  
-      }
-      return(lst)
+      return(unlist(lst_per_sample,recursive=FALSE))
     })
-    lst_models <- do.call("rbind", lst_models)
-    
-    if(details == FALSE){
-      if(per_selector == TRUE){
-        lst_models <- aggregate(lst_models$r_squared, 
-                                by = list(lst_models$model_response,
-                                          lst_models$model_selector),
-                                FUN = mean)
-        colnames(lst_models) <- c("model_response", "model_selector", "R2")
-      } else {
-        lst_models <- aggregate(lst_models$r_squared, 
-                                by = list(lst_models$model_response),
-                                FUN = mean)
-        colnames(lst_models) <- c("model_response", "R2")
-      }
-    }
-    
-  } else {
-    lst_models <- lapply(models, function(m){
-      lst_subruns <- lapply(m, function(r){
-        if(class(r$testing$PREDICTED) == "data.frame"){
-          r$testing$PREDICTED <- r$testing$PREDICTED[,1]
-        }
-        rmse = sqrt(mean((r$testing$PREDICTED - r$testing$RESPONSE)**2))
-        rmse_norm = rmse / mean(r$testing$RESPONSE)
-        smmry <- summary(lm(r$testing$PREDICTED ~ r$testing$RESPONSE))
-        plot(r$testing$PREDICTED ~ r$testing$RESPONSE)
-        r_squared <- smmry$r.squared
-        data.frame(r_squared = r_squared, rmse = rmse, rmse_norm = rmse_norm)
-      })
-      return(do.call("rbind", lst_subruns))
-    })
-    lst_models <- do.call("rbind", lst_models)
-  }
-  
+    return(list(do.call("rbind", sapply(lst_per_response, function(x) x[[1]])),
+                do.call("rbind", sapply(lst_per_response, function(x) x[[2]]))))
+  })
+  names(lst_models) = names(models)
   return(lst_models)
 }
-
